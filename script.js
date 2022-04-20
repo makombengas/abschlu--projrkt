5379250ff897002652fef218f6d014c324f15a1a
Bernard <berlioz2001@yahoo.fr>
2022-04-20 15:44:32 +0200

    Aufgabe erlrdigt und Danke Christoph und Jan Bitte weiter solsls

--- script.js
+++ script.js
@@ -28,42 +28,6 @@ function showSlides(n) {
 
 
 
-let app = {
-	settings: {
-		container: $('calendar'),
-		calendar: $('.front'),
-		days: $('.weeks span'),
-		form: $('.back'),
-		input: $('.back input'),
-		buttons: $('.back button')
-	},
-
-	init: function() {
-		instance = this;
-		settings = this.settings;
-		this.bindUIActions();
-	},
-
-	swap: function(currentSide, desiredSide) {
-		settings.container.toggleClass('flip');
-
-    currentSide.fadeOut(900);
-    currentSide.hide();
-    desiredSide.show();
-
-	},
-
-	bindUIActions: function() {
-		settings.days.on('click', function(){
-			instance.swap(settings.calendar, settings.form);
-			settings.input.focus();
-		});
-
-		settings.buttons.on('click', function(){
-			instance.swap(settings.form, settings.calendar);
-		});
-	}
-}
 
 function myFunction() {
 	var x = document.getElementById("menu");
@@ -73,6 +37,30 @@ function myFunction() {
 	  x.style.display = "block";
 	}
   }
+
+
+
+  var modal = document.getElementById("myModal");
+
+  // Get the button that opens the modal
+  var btn = document.getElementById("myBtn");
+  
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("Xclose")[0];
+  
+ 
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+	modal.style.display = "none";
+  }
+  
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+	if (event.target == modal) {
+	  modal.style.display = "none";
+	}
+  }
+
 
 
 
